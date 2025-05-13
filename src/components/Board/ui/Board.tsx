309e2d40078efb5d252d86b2cf6b6760b2998c45
Артур Мыльников <kuraidatenshidesu@gmail.com>
2025-05-13 18:09:59 +0300

    Created basic dnd logic

--- src/components/Board/ui/Board.tsx
+++ src/components/Board/ui/Board.tsx
@@ -1,10 +1,102 @@
-import type { FC } from "react";
+import { useState, type FC } from "react";
 
 import style from "./Board.module.css";
+import { useAppDispatch, useAppSelector } from "../../../app/store/appStore";
+import { Column } from "../../Column";
+import { DndContext, DragOverlay, pointerWithin, type DragOverEvent, type DragStartEvent } from "@dnd-kit/core";
+import { SortableContext } from "@dnd-kit/sortable";
+import type { TColumn, TTask } from "../../../shared/types/types";
+import { Task } from "../../Task";
+import { changeColumnPosition, changeTaskColumn, changeTaskPosition } from "../../../shared/store/tasksSlice";
 
 const Board: FC = () => {
+  const columns = useAppSelector((state) => state.tasks.columns);
+  const dispatch = useAppDispatch();
+
+  const [draggingItem, setDraggingItem] = useState<{ item: TTask | TColumn; type: "column" | "task" } | null>(null);
+
+  const handleDragStart = (e: DragStartEvent) => {
+    const { active } = e;
+    const { current } = active.data;
+
+    if (!current) return;
+
+    const activeType = current.type;
+
+    if (activeType === "column") {
+      const column = columns.find((column) => column.id === active.id);
+
+      if (!column) return;
+
+      setDraggingItem({ item: column, type: activeType });
+    }
+
+    if (activeType === "task") {
+      const column = columns.find((column) => column.tasks.some((task) => task.id === active.id));
+
+      if (!column) return;
+
+      const task = column.tasks.find((task) => task.id === active.id);
+
+      if (!task) return;
+
+      setDraggingItem({ item: task, type: activeType });
+    }
+  };
+
+  const handleDragOver = (e: DragOverEvent) => {
+    const { active, over } = e;
+
+    if (!over) return;
+    if (active.id === over.id) return;
+
+    const params = {
+      activeId: active.id as string,
+      overId: over.id as string,
+    };
+
+    const activeCurrent = active.data.current;
+    const overCurrent = over.data.current;
+
+    if (!activeCurrent || !overCurrent) return;
+
+    if (activeCurrent.type === "column") {
+      dispatch(changeColumnPosition(params));
+    }
+
+    if (activeCurrent.type === "task") {
+      if (overCurrent.type === "column") {
+        dispatch(changeTaskColumn(params));
+      }
+
+      if (overCurrent.type === "task") {
+        dispatch(changeTaskPosition(params));
+      }
+    }
+  };
+
+  const renderDragOverlay = () => {
+    if (!draggingItem) return;
+
+    if (draggingItem.type === "column") {
+      return <Column column={draggingItem.item as TColumn} />;
+    }
+
+    if (draggingItem.type === "task") {
+      return <Task task={draggingItem.item as TTask} />;
+    }
+  };
+
   return (
     <div className={style.Board}>
+      <DndContext onDragStart={handleDragStart} onDragOver={handleDragOver} collisionDetection={pointerWithin}>
+        <SortableContext items={columns}>
+          {columns.map((column) => (
+            <Column key={column.id} column={column} />
+          ))}
+        </SortableContext>
+        <DragOverlay>{renderDragOverlay()}</DragOverlay>
+      </DndContext>
       <button className={style.Button}>Add column</button>
     </div>
   );
--- src/components/Task/ui/Task.tsx
+++ src/components/Task/ui/Task.tsx
@@ -1,11 +1,28 @@
 import type { FC } from "react";
 
 import style from "./Task.module.css";
+import type { TTask } from "../../../shared/types/types";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
-const Task: FC = () => {
+interface TaskProps {
+  task: TTask;
+}
+
+const Task: FC<TaskProps> = ({ task }) => {
+  const { attributes, listeners, setNodeRef, transform, isDragging } = useSortable({
+    id: task.id,
+    data: { type: "task" },
+  });
+
+  const draggingStyle: React.CSSProperties = {
+    transform: transform ? CSS.Translate.toString(transform) : undefined,
+    opacity: isDragging ? 0.5 : 1,
+  };
+
   return (
-    <div className={style.Task}>
-      <p className={style.Name}>Task name</p>
+    <div className={style.Task} {...attributes} {...listeners} ref={setNodeRef} style={draggingStyle}>
+      <p className={style.Name}>{task.name}</p>
       <div className={style.Controls}>
         <button className={style.Button}>edit</button>
         <button className={style.Button}>delete</button>
